b6fab1bae1f7eba049417b5a7d5facdbc7669f6b
Mattias Holmlund <mattias@holmlund.se>
2022-02-26 19:55:03 +0100

    Use table to list users

--- src/routes/user/index.svelte
+++ src/routes/user/index.svelte
@@ -1,10 +1,31 @@
 <script>
+	import { goto } from '$app/navigation';
 	let users = [];
 	fetch('https://jsonplaceholder.typicode.com/users')
 		.then((response) => response.json())
 		.then((json) => (users = json));
 </script>
 
-{#each users as user}
-	<a href="/user/{user.id}">{user.name}</a><br />
-{/each}
+<div class="overflow-x-auto">
+	<table class="table w-full">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th />
+				<th>Name</th>
+				<th>Job</th>
+				<th>Favorite Color</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each users as user}
+				<tr class="cursor-pointer" on:click={() => goto('/user/' + user.id)}>
+					<th>{user.id}</th>
+					<td>{user.name}</td>
+					<td>Quality Control Specialist</td>
+					<td>Blue</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
