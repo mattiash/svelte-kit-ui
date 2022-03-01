770bbd63b05b4dcba7e86fad39d032d2819b4c9e
Mattias Holmlund <mattias@holmlund.se>
2022-03-01 21:49:03 +0100

    Highlight selected menu item

--- src/lib/MenuItem.svelte
+++ src/lib/MenuItem.svelte
@@ -1,21 +1,26 @@
-<script context="module">
-	// const currentPath = writable(window.location.pathname);
-	// export function routeUpdated(params) {
-	// 	currentPath.set(window.location.pathname);
-	// }
-</script>
-
 <script>
 	import { mainMenuOpen } from '$lib/stores';
+	import { page } from '$app/stores';
 	export let title;
 	export let path;
 
 	function closeMenu() {
 		mainMenuOpen.set(false);
 	}
+
+	let active = false;
+	page.subscribe((p) => {
+		active = path === '/' ? p.url.pathname === path : p.url.pathname.startsWith(path);
+	});
 </script>
 
 <!-- class:is-active={($currentPath + '/').startsWith(path + '/')} -->
-<li>
+<li class:active>
 	<a on:click={closeMenu} href={path}>{title}</a>
 </li>
+
+<style>
+	.active {
+		@apply bg-base-200;
+	}
+</style>
