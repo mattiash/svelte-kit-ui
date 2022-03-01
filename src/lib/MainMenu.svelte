1a7abe7550f66906bf3d0abcb0a8d7a1d7462fbf
Mattias Holmlund <mattias@holmlund.se>
2022-03-01 19:12:29 +0100

    Cleanup Menu

--- src/lib/MainMenu.svelte
+++ src/lib/MainMenu.svelte
@@ -1,14 +1,11 @@
 <script>
-	import { slide } from 'svelte/transition';
 	import MenuItem from './MenuItem.svelte';
 	import SubMenu from './SubMenu.svelte';
-	let show1 = false;
-	let show2 = true;
 	let innerHeight = 100;
 </script>
 
 <svelte:window bind:innerHeight />
-<div style="min-height: {innerHeight - 52}px;">
+<div style="min-height: {innerHeight - 52}px;" class="border-r border-b-200 pt-2">
 	<SubMenu title="Admin">
 		<MenuItem title="Users" path="/user" />
 	</SubMenu>
--- src/lib/SubMenu.svelte
+++ src/lib/SubMenu.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
-	export let title;
+	export let title = '';
 	export let show = false;
-	$: console.log('show', title, show);
 </script>
 
-<div class="p-3 menu overflow-y-auto w-80 bg-base-200 text-base-content">
+<div class="p-3 menu overflow-y-auto w-80 text-base-content bg-base-100">
 	<div on:click={() => (show = !show)}>
 		{title}
 		<span class="float-right">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
-				class="h-5 w-5 inline text-right transition-all"
+				class="h-5 w-5 inline text-right transition-all align-top"
 				class:rotate-180={show}
 				fill="none"
 				viewBox="0 0 24 24"
