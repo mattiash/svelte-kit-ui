2a952f5da4e5f602d61518e7e59e18fb1b495d9e
Mattias Holmlund <mattias@holmlund.se>
2022-03-09 22:28:18 +0100

    Add an error-page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+	import type { ErrorLoad } from '@sveltejs/kit';
+	export const load: ErrorLoad = async ({ error, status }) => {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	};
+</script>
+
+<script>
+	export let title;
+</script>
+
+<h1>{title}</h1>
