1325e77c7f00c1e4e01afef04300580c2a34b09e
Mattias Holmlund <mattias@holmlund.se>
2022-03-09 22:28:49 +0100

    Throw an error when fetch fails

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -4,14 +4,30 @@ export type User = {
 };
 
 const JSONPLACEHOLDER = 'https://jsonplaceholder.typicode.com';
+
+async function myFetch(input: RequestInfo, init?: RequestInit) {
+	const resp = await fetch(input, init);
+	if (resp.ok) {
+		return resp;
+	} else {
+		throw new Error(resp.statusText);
+	}
+}
+
 export async function getUsers() {
 	const url = JSONPLACEHOLDER + '/users';
-	const resp = await fetch(url);
+	const resp = await myFetch(url);
 	return (await resp.json()) as User[];
 }
 
 export async function getUser(id: string) {
 	const url = JSONPLACEHOLDER + '/users/' + id;
-	const resp = await fetch(url);
+	const resp = await myFetch(url);
 	return (await resp.json()) as User;
+}
+
+function sleep(ms: number) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
 }
